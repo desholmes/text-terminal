8eab361d0470f9b7a83a192c4173330ade7bb464
desholmes <desholmes@gmail.com>
2021-10-04 09:07:46 +0100

    WIP bumped dev deps, example failing

--- src/modules/defaultCommands.js
+++ src/modules/defaultCommands.js
@@ -1,7 +1,7 @@
-import package from "../../package.json";
+import { Package } from "../../package.json";
 
 export default {
   clear: (terminal) => terminal.clear(),
 
-  version: (terminal) => terminal.output(`Text Terminal v${package.version}`),
+  version: (terminal) => terminal.output(`Text Terminal v${Package.version}`),
 };
